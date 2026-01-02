37021511603aded0dfd3f7260c30aef7342e0a5c
DanielPenda <152607312+DanielPenda@users.noreply.github.com>
2026-01-02 12:22:32 +0100

    changes on css to fit mobile screens better

--- js/data.js
+++ js/data.js
@@ -3,7 +3,6 @@ export const articles = [
   { id:1, name:"T-Shirt", price:35, stock:10, type:"cloth", sizes:["S","M","L","XL"], img:"assets/tshirt.jpeg" },
   { id:2, name:"Key Chain", price:8, stock:20, type:"other", img:"assets/keychain.jpeg" },
   { id:3, name:"Stickers Pack", price:5, stock:30, type:"other", img:"assets/stickers.jpeg" },
-  { id:4, name:"Complete Box", price:45, stock:5, type:"other", img:"assets/box.jpeg" }
 ];
 
 export let cart = JSON.parse(localStorage.getItem("cart")) || [];
