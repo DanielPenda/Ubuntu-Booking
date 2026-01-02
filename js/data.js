78d304f738cb0c360dd695c23059b3982a0d322a
DanielPenda <152607312+DanielPenda@users.noreply.github.com>
2026-01-02 17:56:05 +0100

    good

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 // data.js
 export const articles = [
-  { id:1, name:"T-Shirt", price:40, stock:10, type:"cloth", sizes:["S","M","L","XL"], img:"assets/tshirt.jpeg" },
-  { id:2, name:"Key Chain", price:3.99, stock:20, type:"other", img:"assets/keychain.jpeg" },
-  { id:3, name:"Sticker", price:1.19, stock:30, type:"other", img:"assets/stickers.jpeg" },
-  { id:4, name:"Complete set", price:42.99, stock:5, type:"other", img:" assets/box.jpeg" }
+  { id:1, name:"T-Shirt", price:40, stock:10, type:"cloth", sizes:["S","M","L","XL"], img:"https://danielpenda.github.io/Ubuntu-Booking/assets/tshirt.jpeg" },
+  { id:2, name:"Key Chain", price:3.99, stock:20, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/keychain.jpeg" },
+  { id:3, name:"Sticker", price:1.19, stock:30, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/stickers.jpeg" },
+  { id:4, name:"Complete set", price:42.99, stock:5, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/box.jpeg" }
 ];
 
 export let cart = JSON.parse(localStorage.getItem("cart")) || [];
