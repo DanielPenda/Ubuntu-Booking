7c5e463e2969614823ee22a42dd5660079c40337
daniel <danieldylan124@gmail.com>
2026-01-16 16:04:38 +0100

    commited

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 // data.js
 export const articles = [
-  { id:1, name:"T-Shirt", price:38.98, stock:20, type:"cloth", sizes:["S","M","L","XxL"], img:"https://danielpenda.github.io/Ubuntu-Booking/assets/tshirt.JPG" },
+  { id:1, name:"T-Shirt", price:38.98, stock:20, type:"cloth", sizes:["S","M","L","XXL"], img:"https://danielpenda.github.io/Ubuntu-Booking/assets/tshirt.JPG" },
   { id:2, name:"Key Chain", price:3.99, stock:20, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/keyChain.png" },
   { id:3, name:"Sticker", price:1.19, stock:30, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/stickers.jpeg" },
-  { id:4, name:"Complete set", price:42.99, stock:5, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/set.png" }
+  { id:4, name:"Complete Set", price:42.99, stock:5, type:"other", img:"https://danielpenda.github.io/Ubuntu-Booking/assets/set.png" }
 ];
 
 export let cart = JSON.parse(localStorage.getItem("cart")) || [];
